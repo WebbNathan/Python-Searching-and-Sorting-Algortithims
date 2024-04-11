8d55b2c9c9c01756cf2a5d57534f07af8e7838d7
Nathan Webb <webbnathana@gmail.com>
2024-04-11 16:45:38 -0700

    Start of quickSort

--- Python-Searching-and-Sorting-Algortithims/Python_Searching_and_Sorting_Algortithims.py
+++ Python-Searching-and-Sorting-Algortithims/Python_Searching_and_Sorting_Algortithims.py
@@ -5,25 +5,40 @@ April 11 2024
 '''
 
 def binarySearch(sortedArr, target, leftBound, rightBound):
-   midpoint = (leftBound + rightBound) >> 1
-   while(rightBound - leftBound > 1):
-      if(sortedArr[midpoint] < target):
-         leftBound = midpoint
-         if(rightBound - leftBound < 1): return rightBound
-         midpoint = (leftBound + rightBound) >> 1
-      elif(sortedArr[midpoint] > target):
-         rightBound = midpoint
-         midpoint = (rightBound + leftBound) >> 1
-         if(rightBound - leftBound < 1): return leftBound
-      else:
-         return midpoint
-   return -1 
+    midpoint = (leftBound + rightBound) >> 1
+    while(rightBound - leftBound > 1):
+        if(sortedArr[midpoint] < target):
+            leftBound = midpoint
+            if(rightBound - leftBound < 1): return rightBound
+            midpoint = (leftBound + rightBound) >> 1
+        elif(sortedArr[midpoint] > target):
+            rightBound = midpoint
+            midpoint = (rightBound + leftBound) >> 1
+            if(rightBound - leftBound < 1): return leftBound
+        else: return midpoint
+    return -1 
 
 def quickSortParition(arr, i, k):
-   pass
+    pivot = (i + k) << 1
+    l = i, r = k, temp = 0
+    
+    while(r - l > 1):
+        while(arr[pivot] > arr[l]): ++l
+        while(arr[pivot] < arr[r]): --r
+        
+        if(r - l > 1):
+            temp = arr[l]
+            arr[l] = arr[r]
+            arr[r] = temp
+        ++i
+        --r
+        
+    #Pivot value is new upper bound for left parition and lower bound for high partion
+         
 
 def quickSort(arr, i, k):
-   pass
+    
+    
 
    
     
