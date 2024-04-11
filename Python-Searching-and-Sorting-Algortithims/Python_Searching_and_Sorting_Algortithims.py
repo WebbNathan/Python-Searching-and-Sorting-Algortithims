7e32f67aadb3d5c6b931b73159ad64ea75a18e14
Nathan Webb <webbnathana@gmail.com>
2024-04-11 16:10:17 -0700

    added bitwise operators to replace division by 2, and smoothed out checks

--- Python-Searching-and-Sorting-Algortithims/Python_Searching_and_Sorting_Algortithims.py
+++ Python-Searching-and-Sorting-Algortithims/Python_Searching_and_Sorting_Algortithims.py
@@ -5,23 +5,25 @@ April 11 2024
 '''
 
 def binarySearch(sortedArr, target, leftBound, rightBound):
-   midpoint = (leftBound + rightBound) // 2
+   midpoint = (leftBound + rightBound) >> 1
    while(rightBound - leftBound > 1):
       if(sortedArr[midpoint] < target):
          leftBound = midpoint
-         midpoint = (leftBound + rightBound) // 2
+         if(rightBound - leftBound < 1): return rightBound
+         midpoint = (leftBound + rightBound) >> 1
       elif(sortedArr[midpoint] > target):
          rightBound = midpoint
-         midpoint = (rightBound + leftBound) // 2
+         midpoint = (rightBound + leftBound) >> 1
+         if(rightBound - leftBound < 1): return leftBound
       else:
          return midpoint
-   if(sortedArr[leftBound] == target):
-      return leftBound
-   elif(sortedArr[rightBound] == target):
-      return rightBound
-   else:
-      return -1 #Not found
+   return -1 
 
-def
+def quickSortParition(arr, i, k):
+   pass
+
+def quickSort(arr, i, k):
+   pass
+
    
     
